1c65312fa8522cc6b307852a5a179f6ae1740c2e
zenger7 <nawaaf.almansour@gmail.com>
2019-04-15 07:00:40 +0300

    done hw-w02d01

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
-// turn on stop function 
+
+$(document).ready(function(){
+
+function turnOffLights(light) {
+  if ( (light).attr('id') === 'stopButton'){
+     $('#stopLight').css('background-color' , 'red');
+   }else if ((light).attr('id') === 'slowButton'){
+     $('#slowLight').css('background-color' , 'yellow');
+   }else if ((light).attr('id') === 'goButton'){
+     $('#goLight').css('background-color' , 'green');
+   }
+}
+
+// turn on stop function
+$('.button').on('click', turnOnStopLight);
 function turnOnStopLight() {
     // change background color of stop light to red
     // changes background color of green and yellow to black
+  console.log('fff');
+   if ($(this).attr('id') === 'stopButton'){
+      $('#stopLight').css('background-color' , 'red');
+    }else {
+      $('#stopLight').css('background-color' , 'black');
+    }
 }
+
 
 // turn on slow function
-function turnOnSlowLight() { 
+$('.button').on('click',turnOnSlowLight );
+
+function turnOnSlowLight() {
   // change background color of slow light to yellow
   // changes background color of red and green to black
+   if ($(this).attr('id') === 'slowButton'){
+     $('#slowLight').css('background-color' , 'yellow');
+   }else {
+     $('#slowLight').css('background-color' , 'black');
+   }
 }
-
 // turn on go function
+$('.button').on('click',turnOnGoLight);
+
 function turnOnGoLight() {
     // change background color of go light to green
     // changes background color of red and yellow to black
+    if ($(this).attr('id') === 'goButton'){
+      $('#goLight').css('background-color' , 'green');
+    }else {
+      $('#goLight').css('background-color' , 'black');
+    }
 }
 
 // set stop light click event
+//event listener blah blah
+ turnOffLights($(this));
 
 // set slow light click event
 
 // set go light click event
-
+}) //DON'T WRITE CODE BELOW THIS
